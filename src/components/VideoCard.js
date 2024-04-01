0ce8eff99d6d1c17be2f6d5b2d5be84081098bdf
vignesh <vigneshfornavy@gmail.com>
2024-04-01 12:25:55 +0530

    Added View Hover affect

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { imgPoster } from "../utils/constant";
 
 const VideoCard = ({ img }) => {
-  if (!img) return;
+  const [showView, setShowView] = useState(false);
 
   return (
-    <div className="ml-2 p-2 sm:p-0 cursor-pointer w-36 md:w-48">
-      <img className="rounded-md" src={imgPoster + img}></img>
+    <div 
+      className="ml-2 p-2 sm:p-0 cursor-pointer w-36 md:w-48 transition-transform duration-200 ease-in-out transform sm:hover:scale-150"
+      onMouseEnter={() => setShowView(true)}
+      onMouseLeave={() => setShowView(false)}
+    >
+      {showView && (
+        <div className="hidden sm:absolute top-32 left-16">
+          <span className="text-white font-semibold border p-2 rounded-sm">Watch +</span>
+        </div>
+      )}
+      <img className="rounded-md" src={imgPoster + img} alt="Poster" />
     </div>
   );
 };
