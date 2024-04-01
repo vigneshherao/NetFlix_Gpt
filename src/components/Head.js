14d76f321273d4a01c9c2b0ce799a7968b103fba
vignesh <vigneshfornavy@gmail.com>
2024-04-01 12:40:03 +0530

    Changes in Head

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,73 +4,92 @@ import MenuItem from "@mui/material/MenuItem";
 import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { getAuth,onAuthStateChanged,signOut } from "firebase/auth";
-import { useEffect } from 'react';
-import { addUser, removeUser } from '../utils/userSlice';
-import {userIcon} from "../utils/constant"
-
-
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
+import { userIcon } from "../utils/constant";
+import { FaRegBell } from "react-icons/fa6";
 const Head = () => {
-  const user = useSelector((store)=>store.user)
+  const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-
-  const signOutMehod = ()=>{
+  const signOutMehod = () => {
     const auth = getAuth();
-    signOut(auth).then(() => {
-      dispatch(removeUser());
-      navigate("/");
-    }).catch((error) => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+        navigate("/");
+      })
+      .catch((error) => {
         console.log(error);
-    });
-    
-  }
+      });
+  };
 
-
-  useEffect(()=>{
+  useEffect(() => {
     const auth = getAuth();
     const unSubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        const {displayName,uid,email,photoURL} = user;
-        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}))
-        navigate("/browse")
+        const { displayName, uid, email, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
       } else {
-        dispatch(removeUser())
+        dispatch(removeUser());
         navigate("/");
       }
     });
 
-    return ()=> unSubscribe
-},[])
-
+    return () => unSubscribe;
+  }, []);
 
   return (
     <div className="flex  justify-between absolute z-10  w-[100%] bg-gradient-to-b from-black">
-      <img
-        className="w-52 ml-[8%]"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-      ></img>
-      {
-        user && <div className=" w-52 h-20 flex justify-center items-center">
-        <PopupState variant="popover" popupId="demo-popup-menu">
-          {(popupState) => (
-            <React.Fragment>
-              <img
-                {...bindTrigger(popupState)}
-                className="w-9 h-9 rounded-sm justify-center  cursor-pointer"
-                src={userIcon}
-              ></img>
-              <Menu {...bindMenu(popupState)}>
-                <MenuItem onClick={popupState.close}>{user.displayName}</MenuItem>
-                <MenuItem onClick={popupState.close}>My account</MenuItem>
-                <MenuItem onClick={signOutMehod}>Signout</MenuItem>
-              </Menu>
-            </React.Fragment>
-          )}
-        </PopupState>
+      <div className="flex">
+        <div>
+          <img
+            className="w-52 ml-[5%]"
+            src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          ></img>
+        </div>
+        <div className="hidden sm:flex ml-8">
+          <ul className="flex justify-center items-center gap-8 text-white">
+            <li className="hover:opacity-70 cursor-pointer">Home</li>
+            <li  className="hover:opacity-70 cursor-pointer">Movies</li>
+            <li  className="hover:opacity-70 cursor-pointer">New & Popular</li>
+            <li  className="hover:opacity-70 cursor-pointer">My List</li>
+          </ul>
+        </div>
       </div>
-      }
+      {user && (
+        <div className=" w-52 h-20 flex justify-center items-center gap-6">
+          <div><FaRegBell className="text-white text-xl cursor-pointer" /></div>
+          <PopupState variant="popover" popupId="demo-popup-menu">
+            {(popupState) => (
+              <React.Fragment>
+                <img
+                  {...bindTrigger(popupState)}
+                  className="w-9 h-9 rounded-sm justify-center  cursor-pointer"
+                  src={userIcon}
+                ></img>
+                <Menu {...bindMenu(popupState)}>
+                  <MenuItem onClick={popupState.close}>
+                    {user.displayName}
+                  </MenuItem>
+                  <MenuItem onClick={popupState.close}>My account</MenuItem>
+                  <MenuItem onClick={signOutMehod}>Signout</MenuItem>
+                </Menu>
+              </React.Fragment>
+            )}
+          </PopupState>
+        </div>
+      )}
     </div>
   );
 };
