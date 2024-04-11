9e29cdf67e5772168ea22ba71dac31b9918715f2
vignesh <vigneshfornavy@gmail.com>
2024-04-11 19:09:28 +0530

    Mobile Screen Descreiption Hidden

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -6,7 +6,7 @@ const VideoTitle = ({ title, description }) => {
   return (
     <div className="w-screen aspect-video pt-[25%] md:pt-[20%] px-6 md:px-12 absolute text-white bg-gradient-to-r from-black">
       <h2 className="text-lg md:text-6xl font-netflixSansBold text-white">{title}</h2>
-      <p className=" md:block sm:w-1/4 md:w-1/2 pt-5">{description.slice(0, 180)}</p>
+      <p className="hidden md:block sm:w-1/4 md:w-1/2 pt-5">{description.slice(0, 180)}</p>
       <div className="flex p-2 md:p-0 md:pt-2">
         <button className="font-semibold justify-center items-center hover:bg-opacity-80 px-2 md:px-4 py-2 rounded bg-white text-black  w-[100px] min-w-[100px] md:flex">
           <Link to={`/watch/?name=${encodeURIComponent(title)}`}>{play}</Link>
