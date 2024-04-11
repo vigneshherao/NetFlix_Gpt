3b690d94225ef40828561e92d74e7b560b58bdd7
vignesh <vigneshfornavy@gmail.com>
2024-04-11 23:03:52 +0530

    added full screen in iframe

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -53,7 +53,8 @@ const Watch = () => {
           }?autoplay=1&controls=1&enablejsapi=1&rel=0&version=3${"&mute=0"}`}
           title="YouTube video player"
           autoPlay={true}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen="allowfullscreen"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share,allowfullscreen"
         ></iframe>
         <WatchDetails trailerVideos={trailerVideos}/>
       </div>
